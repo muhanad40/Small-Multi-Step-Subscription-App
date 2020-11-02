764b02a22ffaa806bf7647f760bb93ecd04831fe
Muhanad Al-Rubaiee <muhanad40@gmail.com>
2020-11-02 11:47:37 +0000

    Fix attribute

--- src/components/CategorySelection.tsx
+++ src/components/CategorySelection.tsx
@@ -8,7 +8,7 @@ const CategorySelection = () => {
 				<li>
 					<a href="#" className="card categories__card">
 						<svg className="categories__icon" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40">
-							<path fill-rule="evenodd" clip-rule="evenodd" d="M20 40c11.046 0 20-8.954 20-20S31.046 0 20 0 0 8.954 0 20s8.954 20 20 20zm5-29c0 7.732-6.268 14-14 14v4c9.941 0 18-8.059 18-18h-4z" fill="currentColor"/>
+							<path fillRule="evenodd" clip-rule="evenodd" d="M20 40c11.046 0 20-8.954 20-20S31.046 0 20 0 0 8.954 0 20s8.954 20 20 20zm5-29c0 7.732-6.268 14-14 14v4c9.941 0 18-8.059 18-18h-4z" fill="currentColor"/>
 						</svg>
 
 						<span className="categories__label">Erectile dysfunction</span>
@@ -17,7 +17,7 @@ const CategorySelection = () => {
 				<li>
 					<a href="#" className="card categories__card selected">
 						<svg className="categories__icon" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40">
-							<path fill-rule="evenodd" clip-rule="evenodd" d="M20 40c11.046 0 20-8.954 20-20S31.046 0 20 0 0 8.954 0 20s8.954 20 20 20zm5-29c0 7.732-6.268 14-14 14v4c9.941 0 18-8.059 18-18h-4z" fill="currentColor"/>
+							<path fillRule="evenodd" clip-rule="evenodd" d="M20 40c11.046 0 20-8.954 20-20S31.046 0 20 0 0 8.954 0 20s8.954 20 20 20zm5-29c0 7.732-6.268 14-14 14v4c9.941 0 18-8.059 18-18h-4z" fill="currentColor"/>
 						</svg>
 
 						<span className="categories__label">Erectile dysfunction</span>
@@ -26,7 +26,7 @@ const CategorySelection = () => {
 				<li>
 					<a href="#" className="card categories__card">
 						<svg className="categories__icon" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40">
-							<path fill-rule="evenodd" clip-rule="evenodd" d="M20 40c11.046 0 20-8.954 20-20S31.046 0 20 0 0 8.954 0 20s8.954 20 20 20zm5-29c0 7.732-6.268 14-14 14v4c9.941 0 18-8.059 18-18h-4z" fill="currentColor"/>
+							<path fillRule="evenodd" clip-rule="evenodd" d="M20 40c11.046 0 20-8.954 20-20S31.046 0 20 0 0 8.954 0 20s8.954 20 20 20zm5-29c0 7.732-6.268 14-14 14v4c9.941 0 18-8.059 18-18h-4z" fill="currentColor"/>
 						</svg>
 
 						<span className="categories__label">Erectile dysfunction</span>
