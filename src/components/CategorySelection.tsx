8828053176e684f1cb1b1c6a69b2452d1f3c17df
Muhanad Al-Rubaiee <muhanad40@gmail.com>
2020-11-02 23:09:32 +0000

    Fix styles

--- src/components/CategorySelection.tsx
+++ src/components/CategorySelection.tsx
@@ -6,31 +6,31 @@ const CategorySelection = () => {
 			<h1>What do you need help with?</h1>
 			<ul className="categories">
 				<li>
-					<a href="#" className="card categories__card">
+					<button className="card categories__button">
 						<svg className="categories__icon" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40">
-							<path fillRule="evenodd" clip-rule="evenodd" d="M20 40c11.046 0 20-8.954 20-20S31.046 0 20 0 0 8.954 0 20s8.954 20 20 20zm5-29c0 7.732-6.268 14-14 14v4c9.941 0 18-8.059 18-18h-4z" fill="currentColor"/>
+							<path fillRule="evenodd" clipRule="evenodd" d="M20 40c11.046 0 20-8.954 20-20S31.046 0 20 0 0 8.954 0 20s8.954 20 20 20zm5-29c0 7.732-6.268 14-14 14v4c9.941 0 18-8.059 18-18h-4z" fill="currentColor"/>
 						</svg>
 
 						<span className="categories__label">Erectile dysfunction</span>
-					</a>
+					</button>
 				</li>
 				<li>
-					<a href="#" className="card categories__card selected">
+					<button className="card categories__button">
 						<svg className="categories__icon" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40">
-							<path fillRule="evenodd" clip-rule="evenodd" d="M20 40c11.046 0 20-8.954 20-20S31.046 0 20 0 0 8.954 0 20s8.954 20 20 20zm5-29c0 7.732-6.268 14-14 14v4c9.941 0 18-8.059 18-18h-4z" fill="currentColor"/>
+							<path fillRule="evenodd" clipRule="evenodd" d="M20 40c11.046 0 20-8.954 20-20S31.046 0 20 0 0 8.954 0 20s8.954 20 20 20zm5-29c0 7.732-6.268 14-14 14v4c9.941 0 18-8.059 18-18h-4z" fill="currentColor"/>
 						</svg>
 
 						<span className="categories__label">Erectile dysfunction</span>
-					</a>
+					</button>
 				</li>
 				<li>
-					<a href="#" className="card categories__card">
+					<button className="card categories__button">
 						<svg className="categories__icon" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40">
-							<path fillRule="evenodd" clip-rule="evenodd" d="M20 40c11.046 0 20-8.954 20-20S31.046 0 20 0 0 8.954 0 20s8.954 20 20 20zm5-29c0 7.732-6.268 14-14 14v4c9.941 0 18-8.059 18-18h-4z" fill="currentColor"/>
+							<path fillRule="evenodd" clipRule="evenodd" d="M20 40c11.046 0 20-8.954 20-20S31.046 0 20 0 0 8.954 0 20s8.954 20 20 20zm5-29c0 7.732-6.268 14-14 14v4c9.941 0 18-8.059 18-18h-4z" fill="currentColor"/>
 						</svg>
 
 						<span className="categories__label">Erectile dysfunction</span>
-					</a>
+					</button>
 				</li>
 			</ul>
 		</>
