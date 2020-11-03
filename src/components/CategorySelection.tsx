fe9e42a0152d2722c129b89567558de63c829303
Muhanad Al-Rubaiee <muhanad40@gmail.com>
2020-11-03 10:51:36 +0000

    Allow user to select category and highlight selection

--- src/components/CategorySelection.tsx
+++ src/components/CategorySelection.tsx
@@ -1,29 +1,42 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import classNames from 'classnames';
 
 import { useStoreContext } from '../store';
+import { Category, ActionTypes } from '../store/types';
 
 const CategorySelection = () => {
 	const { state, dispatch } = useStoreContext();
+	const onCategoryClick = useCallback((categoryId: Category['id']) => {
+		dispatch({
+			type: ActionTypes.SET_CATEGORY,
+			payload: categoryId,
+		});
+	}, []);
 
 	return (
 		<>
 			<h1>What do you need help with?</h1>
 			<ul className="categories">
-			{Object.keys(state.categories).map((catKey) => {
-				const category = state.categories[catKey];
+				{Object.keys(state.categories).map((catKey) => {
+					const category = state.categories[catKey];
+					const classes = classNames({
+						card: true,
+						categories__button: true,
+						selected: state.selectedCategoryId === category.id,
+					});
 
-				return (
-					<li>
-						<button className="card categories__button">
-							<svg className="categories__icon" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40">
-								<path fillRule="evenodd" clipRule="evenodd" d="M20 40c11.046 0 20-8.954 20-20S31.046 0 20 0 0 8.954 0 20s8.954 20 20 20zm5-29c0 7.732-6.268 14-14 14v4c9.941 0 18-8.059 18-18h-4z" fill="currentColor"/>
-							</svg>
+					return (
+						<li>
+							<button className={classes} onClick={() => onCategoryClick(category.id)}>
+								<svg className="categories__icon" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40">
+									<path fillRule="evenodd" clipRule="evenodd" d="M20 40c11.046 0 20-8.954 20-20S31.046 0 20 0 0 8.954 0 20s8.954 20 20 20zm5-29c0 7.732-6.268 14-14 14v4c9.941 0 18-8.059 18-18h-4z" fill="currentColor"/>
+								</svg>
 
-							<span className="categories__label">{category.attributes.name}</span>
-						</button>
-					</li>
-				);
-			})}
+								<span className="categories__label">{category.attributes.name}</span>
+							</button>
+						</li>
+					);
+				})}
 			</ul>
 		</>
 	);
