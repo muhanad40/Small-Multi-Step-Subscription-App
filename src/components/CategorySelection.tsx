82bd8308b4d2c3d199ead5e7ed2543e17857ba3c
Muhanad Al-Rubaiee <muhanad40@gmail.com>
2020-11-03 12:34:20 +0000

    TODO comment

--- src/components/CategorySelection.tsx
+++ src/components/CategorySelection.tsx
@@ -28,6 +28,7 @@ const CategorySelection = () => {
 					return (
 						<li>
 							<button className={classes} onClick={() => onCategoryClick(category.id)}>
+								{/* TODO: This is a placeholder. Use correct icons for each category */}
 								<svg className="categories__icon" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40">
 									<path fillRule="evenodd" clipRule="evenodd" d="M20 40c11.046 0 20-8.954 20-20S31.046 0 20 0 0 8.954 0 20s8.954 20 20 20zm5-29c0 7.732-6.268 14-14 14v4c9.941 0 18-8.059 18-18h-4z" fill="currentColor"/>
 								</svg>
