c965c53708eb84c80e1ee39b2a495edcb797d3ba
Muhanad Al-Rubaiee <muhanad40@gmail.com>
2020-11-04 14:51:23 +0000

    Post even when clicking Next button

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,77 @@
-import React from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import { useStoreContext } from '../store';
-import { ActionTypes, Steps } from '../store/types';
+import { ActionTypes, Steps, Product, ProductVariant, Category } from '../store/types';
 
+enum EventTypes {
+	USER_STARTED_FLOW = 'user-started-flow',
+	USER_SELECTED_CATEGORY = 'user-selected-category',
+	USER_SELECTED_PRODUCT = 'user-selected-product',
+	USER_ENTERED_CONTACT_DATA = 'user-entered-contact-data',
+	USER_SUBMITTED_ORDER = 'user-submitted-order',
+}
+
+type StepToEventType = {
+	[key in Steps]?: EventTypes;
+}
+
+const stepToEventMap: StepToEventType = {
+	[Steps.CATEGORY_SELECTION]: EventTypes.USER_SELECTED_CATEGORY,
+	[Steps.VARIANT_SELECTION]: EventTypes.USER_SELECTED_PRODUCT,
+	[Steps.CONTACT_DETAILS_FORM]: EventTypes.USER_ENTERED_CONTACT_DATA,
+	[Steps.ORDER_SUMMARY]: EventTypes.USER_SUBMITTED_ORDER,
+}
+
+interface StepDataResult {
+	foo?: 'bar',
+	selectedCategory?: Category;
+	selectedProduct?: Product;
+	selectedProductVariant?: ProductVariant;
+	contactDetails?: {},
+}
 
 const Footer = () => {
 	const [nextButtonLabel, setNextButtonLabel] = useState<string>('Next');
 	const { state, dispatch } = useStoreContext();
+	const [collectedUserData, setCollectedUserData] = useState<StepDataResult>({});
+	const getStepData = useCallback((step: Steps | null) => {
+		let result: StepDataResult = {};
+
+		setCollectedUserData((collectedUserData) => ({
+			...collectedUserData,
+			...result,
+		}));
+
+		if (!step) {
+			result.foo = 'bar';
+		} else if (step === Steps.CATEGORY_SELECTION) {
+			result.selectedCategory = state.categories[state.selectedCategoryId];
+		} else if (step === Steps.VARIANT_SELECTION) {
+			result.selectedProduct =state.products[state.selectedProductId];
+			result.selectedProductVariant =state.productVariants[state.selectedProductVariantId];
+		} else if (step === Steps.CONTACT_DETAILS_FORM) {
+			result.contactDetails = state.contactDetails;
+		} else if (step === Steps.ORDER_SUMMARY) {
+			result = collectedUserData;
+		}
+
+		return result;
+	}, [state, collectedUserData, setCollectedUserData]);
 	const onNextClick = useCallback(() => {
+		fetch('https://testapi.numan.com/v1/events', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				type: !state.currentStep ? EventTypes.USER_STARTED_FLOW : stepToEventMap[state.currentStep],
+				user_id: state.userId,
+				data: getStepData(state.currentStep),
+			}),
+		});
 		dispatch({
 			type: ActionTypes.NEXT_STEP,
 		});
-	}, [state, dispatch]);
+	}, [state, dispatch, getStepData]);
 	const onPreviousClick = useCallback(() => {
 		dispatch({
 			type: ActionTypes.PREVIOUS_STEP,
